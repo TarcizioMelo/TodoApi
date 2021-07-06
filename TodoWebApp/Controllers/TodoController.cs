05d0b5bf1d2df4af40610db003c3868ce3eb9cf2
LoneTod <tarcizio_melo@hotmail.com>
2021-07-06 00:32:40 -0300

    Adding Client

---
+++ TodoWebApp/Controllers/TodoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TodoWebApp.Models;
+
+namespace TodoWebApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            IEnumerable<TodoItem> item = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44331/api/");
+                var responseTask = client.GetAsync("TodoItems");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                Console.WriteLine(result);
+                if (result.IsSuccessStatusCode)
+                {
+                    var ReadJob = result.Content.ReadAsAsync<IList<TodoItem>>();
+                    ReadJob.Wait();
+                    item = ReadJob.Result;
+
+                }
+                else
+                {
+                    item = Enumerable.Empty<TodoItem>();
+                    ModelState.AddModelError(string.Empty, "Server Error");
+                    
+                }
+
+            
+            }
+            return View(item);
+        }
+    }
+}
---
+++ TodoWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TodoWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
